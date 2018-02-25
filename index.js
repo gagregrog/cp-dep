876619cd3c0ae43a8f18d3d3845e756edf707782
Robert Reed <robert.mc.reed@gmail.com>
2018-02-24 21:55:58 -0800

    added argv with proper ending

--- index.js
+++ index.js
@@ -2,9 +2,18 @@
 
 const copy = require('clipboardy');
 const getDependencies = require('./lib/get-dependencies.js');
+let path = process.argv[2];
+
+if (!path) throw new Error('Please provide a path');
+
+if (!(/.*package.json$/).test(path)) {
+  if (!path.endsWith('/')) path += '/';
+  path += 'package.json';
+}
+
+console.log(path);
 
 getDependencies('./package.json')
   .then(obj => {
     copy.write(obj.devDependencies + '; ' + obj.dependencies);
   });
-
