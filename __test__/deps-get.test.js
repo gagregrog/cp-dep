26e3f5e01db1451c324e4f5b683edfc9c8edb67f
Robert Reed <robert.mc.reed@gmail.com>
2018-02-25 13:18:25 -0800

    added #get tests

---
+++ __test__/deps-get.test.js
@@ -0,0 +1,106 @@
+'use strict';
+
+require('jest');
+
+const deps = require('../lib/deps.js');
+
+describe('lib/deps.js ', () => {
+  const assets = `${__dirname}/assets`;
+  describe('#get', () => {
+    describe('Asset: both', () => {
+      it('should get both deps', () => {
+        return deps.get(`${assets}/both/package.json`)
+          .then(pkgDeps => {
+            expect(pkgDeps.dependencies).toEqual('dep1 dep2');
+            expect(pkgDeps.devDependencies).toEqual('devDep1 devDep2 devDep3');
+          });
+      });
+      it('should only get full deps', () => {
+        return deps.get(`${assets}/both/package.json`, 'full')
+          .then(pkgDeps => {
+            expect(pkgDeps.dependencies).toEqual('dep1 dep2');
+            expect(pkgDeps.devDependencies).toBeNull();
+          });
+      });
+      it('should only get dev deps', () => {
+        return deps.get(`${assets}/both/package.json`, 'dev')
+          .then(pkgDeps => {
+            expect(pkgDeps.dependencies).toBeNull();
+            expect(pkgDeps.devDependencies).toEqual('devDep1 devDep2 devDep3');
+          });
+      });
+    });
+
+    describe('Asset: full', () => {
+      it('should try to get both deps but only get full', () => {
+        return deps.get(`${assets}/full/package.json`)
+          .then(pkgDeps => {
+            expect(pkgDeps.dependencies).toEqual('dep1 dep2');
+            expect(pkgDeps.devDependencies).toBeNull();
+          });
+      });
+      it('should only get full deps', () => {
+        return deps.get(`${assets}/full/package.json`, 'full')
+          .then(pkgDeps => {
+            expect(pkgDeps.dependencies).toEqual('dep1 dep2');
+            expect(pkgDeps.devDependencies).toBeNull();
+          });
+      });
+      it('should not be able to get dev deps', () => {
+        return deps.get(`${assets}/full/package.json`, 'dev')
+          .then(pkgDeps => {
+            expect(pkgDeps.dependencies).toBeNull();
+            expect(pkgDeps.devDependencies).toBeNull();
+          });
+      });
+    });
+
+    describe('Asset: dev', () => {
+      it('should try to get both deps but only get dev', () => {
+        return deps.get(`${assets}/dev/package.json`)
+          .then(pkgDeps => {
+            expect(pkgDeps.dependencies).toBeNull();
+            expect(pkgDeps.devDependencies).toEqual('devDep1 devDep2 devDep3');
+          });
+      });
+      it('should not be able to get full deps', () => {
+        return deps.get(`${assets}/dev/package.json`, 'full')
+          .then(pkgDeps => {
+            expect(pkgDeps.dependencies).toBeNull();
+            expect(pkgDeps.devDependencies).toBeNull();
+          });
+      });
+      it('should only get dev deps', () => {
+        return deps.get(`${assets}/dev/package.json`, 'dev')
+          .then(pkgDeps => {
+            expect(pkgDeps.dependencies).toBeNull();
+            expect(pkgDeps.devDependencies).toEqual('devDep1 devDep2 devDep3');
+          });
+      });
+    });
+
+    describe('Asset: none', () => {
+      it('should try to get both deps but get none', () => {
+        return deps.get(`${assets}/none/package.json`)
+          .then(pkgDeps => {
+            expect(pkgDeps.dependencies).toBeNull();
+            expect(pkgDeps.devDependencies).toBeNull();
+          });
+      });
+      it('should not be able to get full deps', () => {
+        return deps.get(`${assets}/none/package.json`, 'full')
+          .then(pkgDeps => {
+            expect(pkgDeps.dependencies).toBeNull();
+            expect(pkgDeps.devDependencies).toBeNull();
+          });
+      });
+      it('should not be able to get dev deps', () => {
+        return deps.get(`${assets}/none/package.json`, 'dev')
+          .then(pkgDeps => {
+            expect(pkgDeps.dependencies).toBeNull();
+            expect(pkgDeps.devDependencies).toBeNull();
+          });
+      });
+    });
+  });
+});
--- index.js
+++ index.js
@@ -2,29 +2,6 @@
 
 'use strict';
 
-const copy = require('clipboardy');
-const getDependencies = require('./lib/get-dependencies.js');
+const switchboard = require('./lib/switchboard.js');
 
-let path = process.argv[2];
-
-const log = input => {
-  if (input) console.log(input);
-};
-
-if (!path) throw new Error('Please provide a path');
-
-if (!(/.*package.json$/).test(path)) {
-  if (!path.endsWith('/')) path += '/';
-  path += 'package.json';
-}
-
-getDependencies(path)
-  .then(obj => {
-    copy.write(obj.devDependencies + '; ' + obj.dependencies);
-    log(obj.dependencies);
-    log(obj.devDependencies);
-  })
-  .catch(error => {
-    if (error.code === 'ENOENT') console.log(`package.json not found at path ${path}`);
-    else console.log(error);
-  });
+switchboard('.', { type: 'both' });
