9927983c3f6f8fff0d1a0dd6072188cd6e553f1a
Robert Reed <robert.mc.reed@gmail.com>
2018-02-24 22:30:27 -0800

    fixed join error

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const copy = require('clipboardy');
 const getDependencies = require('./lib/get-dependencies.js');
 let path = process.argv[2];
 
+const log = input => {
+  if (input) console.log(input);
+};
+
 if (!path) throw new Error('Please provide a path');
 
 if (!(/.*package.json$/).test(path)) {
@@ -11,9 +15,13 @@ if (!(/.*package.json$/).test(path)) {
   path += 'package.json';
 }
 
-console.log(path);
-
-getDependencies('./package.json')
+getDependencies(path)
   .then(obj => {
     copy.write(obj.devDependencies + '; ' + obj.dependencies);
+    log(obj.dependencies);
+    log(obj.devDependencies);
+  })
+  .catch(error => {
+    if (error.code === 'ENOENT') console.log(`package.json not found at path ${path}`);
+    else console.log(error);
   });
