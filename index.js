b712a90044f2178b1079c39d31501f2830c48d3a
Robert Reed <robert.mc.reed@gmail.com>
2018-02-25 17:59:07 -0800

    fixed defaultValues

--- index.js
+++ index.js
@@ -2,11 +2,42 @@
 
 'use strict';
 
+const commandLineArgs = require('command-line-args');
 const switchboard = require('./lib/switchboard.js');
 
 const path = '.';
+const optionDefinitions = [
+  { 
+    name: 'copy',
+    alias: 'c',
+    type: Boolean,
+    defaultValue: false,
+  },
+  { 
+    name: 'install',
+    alias: 'i',
+    type: Boolean,
+    defaultValue: false,
+  },
+  { 
+    name: 'supress',
+    alias: 'x',
+    type: Boolean,
+    defaultValue: false,
+  },
+  { 
+    name: 'type',
+    alias: 't',
+    type: String,
+    defaultValue: 'both',
+  },
+];
 
-switchboard(path, { type: 'both' })
+const options = commandLineArgs(optionDefinitions);
+
+console.log(options);
+
+switchboard(path, { type: 'both', copy: true, suppress: false })
   .catch(error => {
     if (error.code === 'ENOENT') console.log(`package.json not found at path ${path}`);
   });
