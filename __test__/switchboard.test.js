beee55ded72686c440bbfb314835c1ed767f7142
Robert Reed <robert.mc.reed@gmail.com>
2018-03-01 11:36:14 -0800

    tests passing with new clipboard package

--- __test__/switchboard.test.js
+++ __test__/switchboard.test.js
@@ -1,22 +1,41 @@
 'use strict';
 
 require('jest');
+const ncp = require('copy-paste');
 
 const switchboard = require('../lib/switchboard.js');
 
 const assets = `${__dirname}/assets`;
 
 describe('./lib/switchboard.js', () => {
-  it('should throw an error if not called with a path', () => {
-    expect(switchboard).toThrow('Please provide a path');
+  // reset clipboard before each test
+  beforeEach((done) => {
+    ncp.copy('', done);
   });
 
   it('should throw if called with an invalid path', () => {
     const path = './packageSpot/package.json';
-    return switchboard(path, { type: 'both' })
+    return switchboard(path, { type: 'both', suppress: true })
       .then(Promise.reject)
       .catch(error => {
         expect(error.code).toEqual('ENOENT');
       });
+  });
+  
+  it('should add /package.json to the path if the path ends in a directory', () => {
+    const path = `${assets}/both`;
+    expect(() =>
+      switchboard(path, { type: 'both', suppress: true })
+    ).not.toThrow();
+  });
+
+  it('should copy to the clipboard given the copy option', (done) => {
+    const path = `${assets}/dev/package.json`;
+    const test = () => {
+      let result = ncp.paste();
+      expect(result).toEqual('npm i devDep1 devDep2 devDep3 -D');
+      done();
+    };
+    return switchboard(path, { type: 'dev', copy: true, suppress: true }, test);
   });
 });
